8349a07f9bc0bd3622f3236656bdecacd5532f3c
MKmuneebkhalid <muneebkhalid006@gmail.com>
2026-02-11 01:21:01 +0100

    fix: multiple calls

--- hooks/useCompanies.ts
+++ hooks/useCompanies.ts
@@ -43,6 +43,8 @@ export function useCompanies() {
   } = useQuery<Company[], Error>({
     queryKey: ['companies'],
     queryFn: fetchCompanies,
+    staleTime: 5 * 60 * 1000,
+    refetchOnWindowFocus: false,
   })
 
   const createCompanyMutation = useMutation({
